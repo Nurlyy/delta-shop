d5980e46e3f84a1dedfdfa80a21ebdbd0b4b9c4b
Nurlytan <nurik.bro.2000@mail.ru>
2023-05-10 15:25:19 +0600

    related products

--- frontend/controllers/ProductController.php
+++ frontend/controllers/ProductController.php
@@ -3,29 +3,40 @@
 namespace frontend\controllers;
 
 use common\models\Products;
+use common\models\Rubrik;
 use yii\filters\AccessControl;
 use yii\web\Controller;
 
-class ProductController extends Controller {
+class ProductController extends Controller
+{
     public $layout = 'layout';
-    public function behaviors(){
+    public function behaviors()
+    {
         $behaviors = parent::behaviors();
 
         // $behaviors['access'] = [
         //     'class' => AccessControl::class,
-            
+
         // ];
 
         return $behaviors;
     }
 
-    public function actionIndex(){
+    public function actionIndex()
+    {
         return $this->redirect('/');
     }
 
-    public function actionProduct(){
+    public function actionProduct()
+    {
         $product_id = htmlentities($_GET['product_id']);
         $product = Products::find()->where(['product_id' => $product_id])->one();
-        return $this->render('product', ['product' => $product]);
+        $rubrik = Rubrik::find()->where(['rubrik_id' => $product->rubrik_id])->one();
+        $products = Products::find()
+            ->where(['rubrik_id' => $rubrik->rubrik_id])
+            ->andWhere(['<>', 'product_id', $product_id])
+            ->limit(4)
+            ->all();
+        return $this->render('product', ['product' => $product, 'products' => $products]);
     }
 }
