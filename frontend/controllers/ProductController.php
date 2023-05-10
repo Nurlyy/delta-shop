ef4ffa863f7129dd1b98986571d4ef6013f39e33
Nurlytan <nurik.bro.2000@mail.ru>
2023-05-10 15:50:48 +0600

    Manufacturer, Characteristics, Category of product

--- frontend/controllers/ProductController.php
+++ frontend/controllers/ProductController.php
@@ -2,10 +2,14 @@
 
 namespace frontend\controllers;
 
+use common\models\Categories;
 use common\models\Products;
 use common\models\Rubrik;
 use yii\filters\AccessControl;
 use yii\web\Controller;
+use common\models\Manufacturers;
+use common\models\ProductCharacteristics;
+use common\models\Subcategories;
 
 class ProductController extends Controller
 {
@@ -32,11 +36,23 @@ class ProductController extends Controller
         $product_id = htmlentities($_GET['product_id']);
         $product = Products::find()->where(['product_id' => $product_id])->one();
         $rubrik = Rubrik::find()->where(['rubrik_id' => $product->rubrik_id])->one();
+        $subcategory = Subcategories::find()->where(['subcategory_id' => $rubrik->subcategory_id])->one();
+        $category = Categories::find()->where(['category_id' => $subcategory->category_id])->one();
+        $manufacturer = Manufacturers::find()->where(['manufacturer_id' => $product->manufacturer_id])->one();
+        $characteristics = ProductCharacteristics::find()->where(['product_id'=>$product->product_id])->all();
         $products = Products::find()
             ->where(['rubrik_id' => $rubrik->rubrik_id])
             ->andWhere(['<>', 'product_id', $product_id])
             ->limit(4)
             ->all();
-        return $this->render('product', ['product' => $product, 'products' => $products]);
+        return $this->render('product', [
+            'product' => $product,
+            'products' => $products,
+            'manufacturer' => $manufacturer,
+            'rubrik' => $rubrik,
+            'subcategory' => $subcategory,
+            'category' => $category,
+            'characteristics' => $characteristics
+        ]);
     }
 }
--- frontend/views/product/product.php
+++ frontend/views/product/product.php
@@ -10,13 +10,21 @@
             <div class="col-md-6">
 
                 <h1 class="display-5 fw-bolder"><?= $product->product_name ?></h1>
-                <div class="fs-5 mb-5">
+                <div class="fs-5 mb-3">
                     <!-- <span class="text-decoration-line-through">$1888</span> -->
-                    <span>$<?= $product->price ?></span>
+                    <span>Price: <strong> $<?= $product->price ?></strong></span>
                 </div>
-                <div class="fs-5 mb-5">
+                <div class="fs-5 mb-3">
                     <!-- <span class="text-decoration-line-through">$1888</span> -->
-                    <span>In stock: <?= $product->count ?> pcs.</span>
+                    <span>In stock: <strong><?= $product->count ?></strong> pcs.</span>
+                </div>
+                <div class="fs-5 mb-3">
+                    <!-- <span class="text-decoration-line-through">$1888</span> -->
+                    <span>Category: <strong><?= $category->category_name ?></strong> / <strong><?= $subcategory->subcategory_name ?></strong> / <strong><?= $rubrik->rubrik_name ?></strong></span>
+                </div>
+                <div class="fs-5 mb-3">
+                    <!-- <span class="text-decoration-line-through">$1888</span> -->
+                    <span>Manufacturer: <strong><?= $manufacturer->manufacturer_name ?></strong></span>
                 </div>
                 <p class="lead"><?= $product->description ?></p>
                 <div class="d-flex">
@@ -28,39 +36,59 @@
                 </div>
 
             </div>
+            <div class="col-md-12">
+
+                <h4 class="display-5 fw-bolder text-center">Characteristics</h4>
+
+                <div>
+                    <?php foreach($characteristics as $ch){ ?>
+                    <div class="card card-body">
+                        <div class="row">
+                            <div class="col-6 text-center">
+                                <?= $ch->key ?>
+                            </div>
+                            <div class="col-6 text-center">
+                                <?= $ch->value ?>
+                            </div>
+                        </div>
+                    </div>
+                    <?php } ?>
+                </div>
+
+            </div>
         </div>
     </div>
 </section>
 <!-- Related items section-->
-<?php if(!empty($products)){ ?>
-<section class="py-5 bg-light">
-    <div class="container px-4 px-lg-5 mt-5">
-        <h2 class="fw-bolder mb-4">Related products</h2>
-        <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-            <?php foreach($products as $p){ ?>
-            <div class="col mb-5">
-                <div class="card h-100">
-                    <!-- Product image-->
-                    <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                    <!-- Product details-->
-                    <div class="card-body p-4">
-                        <div class="text-center">
-                            <!-- Product name-->
-                            <h5 class="fw-bolder"><?php echo $p->product_name ?></h5>
-                            <!-- Product price-->
-                            <?php echo $p->price ?>
+<?php if (!empty($products)) { ?>
+    <section class="py-5 bg-light">
+        <div class="container px-4 px-lg-5 mt-5">
+            <h2 class="fw-bolder mb-4">Related products</h2>
+            <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                <?php foreach ($products as $p) { ?>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Product image-->
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder"><?php echo $p->product_name ?></h5>
+                                    <!-- Product price-->
+                                    <?php echo $p->price ?>
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+                            </div>
                         </div>
                     </div>
-                    <!-- Product actions-->
-                    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                        <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div>
-                    </div>
-                </div>
+                <?php } ?>
             </div>
-            <?php } ?>
         </div>
-    </div>
-</section>
+    </section>
 <?php } ?>
 
 <!-- <section class="py-5 bg-light">
